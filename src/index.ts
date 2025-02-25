d0522ebc6aecce456b50856febe623991232da9a
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2025-02-25 22:04:25 +0100

    feat(src): implement translation feature using OpenAI API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-const a = 2;
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import { ChatOpenAI } from "@langchain/openai";
+
+const model = new ChatOpenAI({ model: "gpt-3.5-turbo" });
+
+const messages = [
+	new SystemMessage("Translate the following from English into French"),
+	new HumanMessage("hi!"),
+];
+
+const res = await model.invoke(messages);
