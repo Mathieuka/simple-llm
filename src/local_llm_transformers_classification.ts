c94757784d2c23f534e3396d62068f8c16e349b7
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2025-03-05 22:11:51 +0100

    refactor(classification): update local LLM transformers classification
    - Update `getInstance` method to remove `progress_callback` parameter
    - Add `env.cacheDir` assignment to `./cache`
    - Update `response` assignment to use "Bad" instead of "Try it!" as input for classification

--- src/local_llm_transformers_classification.ts
+++ src/local_llm_transformers_classification.ts
@@ -2,20 +2,23 @@ import {
 	type PipelineType,
 	type ProgressCallback,
 	type TextClassificationPipeline,
+	env,
 	pipeline,
 } from "@huggingface/transformers";
+
+env.cacheDir = "./cache";
 
 class MyClassificationPipeline {
 	private task: PipelineType = "text-classification";
 	private model = "Xenova/distilbert-base-uncased-finetuned-sst-2-english";
 	private instance: Promise<TextClassificationPipeline> | undefined = undefined;
 
-	async getInstance(
-		progress_callback: ProgressCallback | undefined = undefined,
-	) {
+	async getInstance() {
 		if (this.instance === undefined) {
 			this.instance = pipeline(this.task, this.model, {
-				progress_callback,
+				progress_callback: () => {
+					// console.log("progress : ", progressInfo);
+				},
 				dtype: "fp32",
 			}) as Promise<TextClassificationPipeline>;
 		}
@@ -26,5 +29,5 @@ class MyClassificationPipeline {
 
 const instance = await new MyClassificationPipeline().getInstance();
 
-const response = await instance("Try it!");
+const response = await instance("Bad");
 console.log(response);
